16737369ce2f71559c32e25b1a3df50693bcb47e
Dayles1 <sololeveling0025@gmail.com>
2025-02-19 18:46:29 +0500

    name for Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'checkAuth' => \App\Http\Middleware\AuthMiddleware::class,
+            'checkUnAuth' => \App\Http\Middleware\UnAuthMiddleware::class,
+
+            
+
+        ]);
+        //UnAuthMiddleware
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
